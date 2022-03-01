55687bcbd587ed3b580241a8f3dd950edc0fc096
lsh23 <shseoul14@gmai.com>
2022-03-01 21:51:51 +0900

    chore : Add Lombok getter & setter to domain

--- src/main/java/com/blog/demo/domain/PostTag.java
+++ src/main/java/com/blog/demo/domain/PostTag.java
@@ -1,8 +1,12 @@
 package com.blog.demo.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter
 public class PostTag {
     @Id @Column(name="POST_TAG_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
