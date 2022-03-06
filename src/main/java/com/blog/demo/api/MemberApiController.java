a57bc7e711d1a568a123f0f8f480104737dc62be
lsh23 <shseoul14@gmai.com>
2022-03-06 20:37:39 +0900

    feat : implement signUP function

---
+++ src/main/java/com/blog/demo/api/MemberApiController.java
@@ -0,0 +1,101 @@
+package com.blog.demo.api;
+
+import com.blog.demo.domain.Member;
+import com.blog.demo.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> memberDtos = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(memberDtos.size(), memberDtos);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        String id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        String id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") String id,
+            @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class CreateMemberResponse {
+        private String id;
+    }
+
+    @Data
+    private class CreateMemberRequest {
+        private String id;
+        private String name;
+        private String password;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class UpdateMemberResponse {
+        private String String;
+        private String name;
+    }
+
+    @Data
+    private class UpdateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+
+}
