30d25ca060f0eeaf7f91d1159745ad71916439ec
lsh23 <shseoul14@gmail.com>
2022-06-14 21:35:36 +0900

    refactor: service layer 분리

--- src/main/java/com/blog/demo/api/CommentApiController.java
+++ src/main/java/com/blog/demo/api/CommentApiController.java
@@ -1,23 +1,17 @@
 package com.blog.demo.api;
 
+import com.blog.demo.api.dto.Result;
 import com.blog.demo.api.dto.comment.CommentDto;
 import com.blog.demo.api.dto.comment.CreateCommentRequest;
 import com.blog.demo.api.dto.comment.CreateCommentResponse;
-import com.blog.demo.domain.Comment;
-import com.blog.demo.domain.Member;
-import com.blog.demo.domain.Post;
 import com.blog.demo.service.CommentService;
 import com.blog.demo.service.MemberService;
 import com.blog.demo.service.PostService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,54 +23,15 @@ public class CommentApiController {
 
     @GetMapping
     public Result getComments(@RequestParam(value="postId", required = false) Long postId){
-
-        List<Comment> comments;
-        if (postId != null){
-            comments = commentService.findAllByPostId(postId);
-        }
-        else{
-            comments = commentService.findAll();
-        }
-        Stream<Comment> commentStream = comments.stream().filter(c -> c.getParent() == null);
-        List<CommentDto> commentDtos = commentStream.map(c -> new CommentDto(c)).collect(Collectors.toList());
+        List<CommentDto> commentDtos = commentService.findAll(postId);
         Integer totalCommentCount = commentDtos.stream().map(c -> c.getChild().size()).reduce(commentDtos.size(), Integer::sum);
-
         return new Result(totalCommentCount, commentDtos);
     }
 
     @PostMapping
     public CreateCommentResponse createComment(@RequestBody @Valid CreateCommentRequest createCommentRequest){
-
-        String memberId = createCommentRequest.getMemberId();
-        Member findMember = memberService.findOne(memberId);
-        Long postId = createCommentRequest.getPostId();
-        Post findPost = postService.findOne(postId);
-        String text = createCommentRequest.getText();
-
-        Comment comment = Comment.builder()
-                .member(findMember)
-                .post(findPost)
-                .text(text)
-                .build();
-
-        Long parentId = createCommentRequest.getParentId();
-        if (parentId != null) {
-            Comment findComment = commentService.findOne(parentId);
-            comment.assignParent(findComment);
-            commentService.save(comment);
-            return new CreateCommentResponse(comment.getId(), comment.getPost().getId(), comment.getMember().getId(), comment.getParent().getId());
-        }
-
-        commentService.save(comment);
-        return new CreateCommentResponse(comment.getId(), comment.getPost().getId(), comment.getMember().getId(), null);
-        
-    }
-
-    @Data
-    @AllArgsConstructor
-    class Result<T> {
-        private int count;
-        private T data;
+        CommentDto commentDto = commentService.createComment(createCommentRequest);
+        return new CreateCommentResponse(commentDto.getId(), commentDto.getMemberId(), null);
     }
 
 }
--- src/main/java/com/blog/demo/service/CommentService.java
+++ src/main/java/com/blog/demo/service/CommentService.java
@@ -1,20 +1,31 @@
 package com.blog.demo.service;
 
+import com.blog.demo.api.dto.comment.CommentDto;
+import com.blog.demo.api.dto.comment.CreateCommentRequest;
 import com.blog.demo.domain.Comment;
+import com.blog.demo.domain.Member;
+import com.blog.demo.domain.Post;
 import com.blog.demo.repository.CommentRepository;
+import com.blog.demo.repository.MemberRepository;
+import com.blog.demo.repository.PostRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
 
-    public CommentService(CommentRepository commentRepository){
+    public CommentService(CommentRepository commentRepository, MemberRepository memberRepository, PostRepository postRepository) {
         this.commentRepository = commentRepository;
+        this.memberRepository = memberRepository;
+        this.postRepository = postRepository;
     }
 
     public Long save(Comment comment){
@@ -28,12 +39,41 @@ public class CommentService {
     }
 
     @Transactional(readOnly = true)
-    public List<Comment> findAll() {
-        return commentRepository.findAll();
+    public List<CommentDto> findAll(Long postId) {
+        return getAllComment(postId).stream()
+                .map(CommentDto::new)
+                .collect(Collectors.toList());
     }
 
-    @Transactional(readOnly = true)
-    public List<Comment> findAllByPostId(Long postId) {
+    private List<Comment> getAllComment(Long postId){
+
+        if (postId == null) {
+            return commentRepository.findAll();
+        }
         return commentRepository.findAllByPostId(postId);
+    }
+
+    public CommentDto createComment(CreateCommentRequest createCommentRequest) {
+        String memberId = createCommentRequest.getMemberId();
+        Member findMember = memberRepository.findOne(memberId);
+        Long postId = createCommentRequest.getPostId();
+        Post findPost = postRepository.findOne(postId);
+        String text = createCommentRequest.getText();
+
+        Comment comment = Comment.builder()
+                .member(findMember)
+                .post(findPost)
+                .text(text)
+                .build();
+
+        Long parentId = createCommentRequest.getParentId();
+
+        if (parentId != null) {
+            Comment findComment = commentRepository.findOne(parentId);
+            comment.assignParent(findComment);
+        }
+
+        commentRepository.save(comment);
+        return new CommentDto(comment);
     }
 }
