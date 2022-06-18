7bfdb5d989b5932b63204c0eb8ece552926cc98c
lsh23 <shseoul14@gmail.com>
2022-06-18 22:46:59 +0900

    refactor: member service layer 분리

--- src/main/java/com/blog/demo/api/MemberApiController.java
+++ src/main/java/com/blog/demo/api/MemberApiController.java
@@ -13,44 +13,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
 
-    @GetMapping("/api/v1/members")
-    public List<Member> membersV1() {
-        return memberService.findMembers();
-    }
-
-    @GetMapping("/api/v2/members")
-    public Result memberV2() {
-        List<Member> findMembers = memberService.findMembers();
-        List<MemberDto> memberDtos = findMembers.stream()
-                .map(m -> new MemberDto(m.getName()))
-                .collect(Collectors.toList());
+    @GetMapping
+    public Result getMembers() {
+        List<MemberDto> memberDtos = memberService.findAll();
         return new Result(memberDtos.size(), memberDtos);
     }
 
-    @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
-        String id = memberService.save(member);
-        return new CreateMemberResponse(id);
+    @PostMapping
+    public ResponseEntity<CreateMemberResponse> createMember(@RequestBody @Valid CreateMemberRequest request) {
+        return ResponseEntity.ok(memberService.createMember(request));
     }
 
-    @PostMapping("api/v2/members")
-    public ResponseEntity<CreateMemberResponse> saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
-        return ResponseEntity.ok(memberService.createUser(request));
-    }
-
-    @PutMapping("api/v2/members/{id}")
-    public UpdateMemberResponse updateMemberV2(
-            @PathVariable("id") String id,
-            @RequestBody @Valid UpdateMemberRequest request) {
-
-        memberService.update(id, request.getName());
-        Member findMember = memberService.findOne(id);
-        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    @PutMapping("/{id}")
+    public UpdateMemberResponse updateMember(
+            @PathVariable("id") String memberId,
+            @RequestBody @Valid UpdateMemberRequest updateMemberRequest) {
+        MemberDto member = memberService.updateMember(memberId, updateMemberRequest);
+        return new UpdateMemberResponse(member.getId(), member.getName());
     }
 
 
