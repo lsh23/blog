034537e003c81734133c08f533efcdab01dbf4a6
lsh23 <shseoul14@gmail.com>
2022-06-18 22:48:27 +0900

    refactor: post service layer 분리

--- src/main/java/com/blog/demo/api/PostApiController.java
+++ src/main/java/com/blog/demo/api/PostApiController.java
@@ -1,18 +1,13 @@
 package com.blog.demo.api;
 
 import com.blog.demo.api.dto.Result;
-import com.blog.demo.api.dto.tag.TagDto;
 import com.blog.demo.api.dto.post.*;
-import com.blog.demo.domain.*;
-import com.blog.demo.repository.PostSearch;
 import com.blog.demo.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,65 +17,34 @@ public class PostApiController {
     private final PostService postService;
 
     @GetMapping
-    public Result getPosts(@RequestParam(value = "id",required = false) String userId,
+    public Result getPosts(@RequestParam(value = "memberId",required = false) String memberId,
                            @RequestParam(value = "categoryId",required = false) Long categoryId){
-
-        PostSearch postSearch = new PostSearch();
-        postSearch.setMemberId(userId);
-        postSearch.setCategoryId(categoryId);
-        List<Post> posts = postService.findPosts(postSearch);
-
-        Stream<Post> postStream = posts.stream();
-
-        List<PostListDto> postListDtos = postStream
-                .map(p -> new PostListDto(p.getId(),p.getTitle(), p.getContent(), p.getCreatedAt(), p.getUpdatedAt(), p.getCategory().getName(), p.getPostTags()))
-                .collect(Collectors.toList());
-
+        List<PostListDto> postListDtos = postService.findPosts(memberId, categoryId);
         return new Result(postListDtos.size(), postListDtos);
-
     }
 
     @GetMapping("/{id}")
     public PostDto getPost(@PathVariable("id") Long id){
-        Post findPost = postService.findOne(id);
-        return new PostDto(findPost.getId(), findPost.getTitle(), findPost.getContent(),findPost.getCreatedAt(), findPost.getUpdatedAt(), findPost.getCategory(), findPost.getPostTags());
+        PostDto postDto = postService.findOne(id);
+        return postDto;
     }
     
     @PostMapping
     public CreatePostResponse createPost(@RequestBody @Valid CreatePostRequest createPostRequest){
-        String title = createPostRequest.getTitle();
-        String contents = createPostRequest.getContents();
-
-        String memberId = createPostRequest.getUserId();
-        Long categoryId = createPostRequest.getCategoryId();
-
-        List<TagDto> tags = createPostRequest.getTags();
-
-        Post post = postService.writePost(title, contents, memberId, categoryId, tags);
-
-        return new CreatePostResponse(post.getId(), post.getTitle());
+        PostDto postDto = postService.createPost(createPostRequest);
+        return new CreatePostResponse(postDto.getId(), postDto.getTitle());
     }
 
     @PatchMapping("/{id}")
     public UpdatePostResponse updatePost(@RequestBody @Valid UpdatePostRequest updatePostRequest, @PathVariable("id") Long id){
-        String title = updatePostRequest.getTitle();
-        String contents = updatePostRequest.getContents();
-
-        String memberId = updatePostRequest.getUserId();
-        Long categoryId = updatePostRequest.getCategoryId();
-
-        List<TagDto> tags = updatePostRequest.getTags();
-
-        Post post = postService.updatePost(id,title, contents, memberId, categoryId, tags);
-
-        return new UpdatePostResponse(post.getId(), post.getTitle());
+        PostDto postDto = postService.updatePost(id,updatePostRequest);
+        return new UpdatePostResponse(postDto.getId(), postDto.getTitle());
     }
 
     @DeleteMapping("/{id}")
     public DeletePostResponse deletePost(@PathVariable("id") Long id){
-        Post post = postService.findOne(id);
-        postService.deleteOne(id);
-        return new DeletePostResponse(post.getId(), post.getTitle());
+        PostDto postDto = postService.deleteOne(id);
+        return new DeletePostResponse(postDto.getId(), postDto.getTitle());
     }
 
 }
