35a17767c3cb86695d7a1c2cbcf8e26bafef65b4
lsh23 <shseoul14@gmail.com>
2022-06-18 22:59:49 +0900

    refactor: postTagServi트ce 업데이

--- src/main/java/com/blog/demo/service/PostTagService.java
+++ src/main/java/com/blog/demo/service/PostTagService.java
@@ -1,23 +1,31 @@
 package com.blog.demo.service;
 
+import com.blog.demo.api.dto.posttag.PostTagDto;
+import com.blog.demo.api.dto.tag.TagDto;
 import com.blog.demo.domain.Post;
 import com.blog.demo.domain.PostTag;
-import com.blog.demo.domain.Tag;
+import com.blog.demo.repository.PostRepository;
 import com.blog.demo.repository.PostTagRepository;
+import com.blog.demo.repository.TagRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class PostTagService {
 
     private final PostTagRepository postTagRepository;
+    private final PostRepository postRepository;
+    private final TagRepository tagRepository;
 
-    public PostTagService(PostTagRepository postTagRepository) {
+    public PostTagService(PostTagRepository postTagRepository, PostRepository postRepository, TagRepository tagRepository) {
         this.postTagRepository = postTagRepository;
+        this.postRepository = postRepository;
+        this.tagRepository = tagRepository;
     }
 
     public Long save(PostTag postTag){
@@ -25,43 +33,65 @@ public class PostTagService {
         return postTag.getId();
     }
 
-    public PostTag findOne(Long id) {
-        return postTagRepository.findOne(id);
+    public PostTagDto findOne(Long id) {
+        PostTag findOne = postTagRepository.findOne(id);
+        return new PostTagDto(findOne);
     }
-    public List<PostTag> findPostTagsByPostId(Long postId){ return postTagRepository.findPostTagsByPostId(postId); }
-    public List<PostTag> findPostTags() {
-        return postTagRepository.findAll();
-    }
-    public void deleteOne(long id) { postTagRepository.deleteOne(id); }
 
-    public List<PostTag> saveByTags(List<Tag> tags){
+    public List<PostTagDto> findPostTags(Long postId) {
+        List<PostTag> all = getPostTags(postId);
+        return all.stream().map(PostTagDto::new).collect(Collectors.toList());
+    }
+
+    private List<PostTag> getPostTags(Long postId){
+        if (postId == null){
+            return postTagRepository.findAll();
+        }
+        return postTagRepository.findPostTagsByPostId(postId);
+    }
+
+    public PostTagDto deleteOne(Long id) {
+        PostTag postTag = postTagRepository.deleteOne(id);
+        return new PostTagDto(postTag);
+    }
+
+    public List<PostTagDto> saveByTags(Long postId, List<TagDto> tagDtos){
         List<PostTag> result = new ArrayList<>();
-        tags.forEach(t->{
-            PostTag newOne = PostTag.builder().tag(t).build();
-            save(newOne);
+        tagDtos.forEach(t->{
+            PostTag newOne = PostTag.builder()
+                    .tag(tagRepository.findOne(t.getId()))
+                    .build();
+            postTagRepository.save(newOne);
             result.add(newOne);
         });
-        return result;
+
+        Post post = postRepository.findOne(postId);
+        result.forEach(pt->pt.assignPost(post));
+        return result.stream().map(PostTagDto::new).collect(Collectors.toList());
     }
 
-    public List<PostTag> updatePostTag(Post post, List<Tag> tags) {
+    public List<PostTagDto> updatePostTag(Long postId, List<TagDto> tagDtos) {
+
+        Post post = postRepository.findOne(postId);
 
         List<PostTag> postTags = post.getPostTags();
         postTags.stream().forEach(pt->{
-            deleteOne(pt.getId());
+            postTagRepository.deleteOne(pt.getId());
         });
 
         postTags.clear();
 
-        tags.forEach(t->{
+        List<PostTag> newPostTags = new ArrayList<>();
+
+        tagDtos.forEach(t->{
             PostTag newOne = PostTag.builder()
-                    .tag(t)
+                    .tag(tagRepository.findOne(t.getId()))
                     .build();
-            save(newOne);
-            postTags.add(newOne);
+            postTagRepository.save(newOne);
+            newPostTags.add(newOne);
         });
 
-        postTags.forEach(pt->pt.assignPost(post));
-        return postTags;
+        newPostTags.forEach(pt->pt.assignPost(post));
+        return newPostTags.stream().map(PostTagDto::new).collect(Collectors.toList());
     }
 }
