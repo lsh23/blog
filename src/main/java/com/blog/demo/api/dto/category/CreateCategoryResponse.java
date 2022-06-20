d1abcc76cec768c83b7f7b12d2ba18e97f4fb487
kihwankim <clack2933@gmail.com>
2022-06-21 01:18:04 +0900

    feat: test code를 위한 default 생성자 추가

--- src/main/java/com/blog/demo/api/dto/category/CreateCategoryResponse.java
+++ src/main/java/com/blog/demo/api/dto/category/CreateCategoryResponse.java
@@ -2,9 +2,11 @@ package com.blog.demo.api.dto.category;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateCategoryResponse {
     private Long id;
     private String name;
