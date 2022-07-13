cad504a4402af182099f269a3d7f68af5176f814
lsh23 <shseoul14@gmail.com>
2022-07-13 23:17:50 +0900

    feat: LoginInterceptor 구현 및 config 등록

---
+++ src/main/java/com/blog/demo/auth/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package com.blog.demo.auth;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    private final JwtUtils jwtUtils;
+
+    public LoginInterceptor(final JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String token = AuthorizationExtractor.extractAccessToken(request);
+        jwtUtils.validateToken(token);
+        return true;
+    }
+
+}
