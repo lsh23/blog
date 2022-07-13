86c1f0da7bb58cc0382651cb6d4a10fa39d31d82
lsh23 <shseoul14@gmail.com>
2022-07-13 23:16:28 +0900

    feat: StringToOauthProviderConverter config 등

---
+++ src/main/java/com/blog/demo/config/OauthConfig.java
@@ -0,0 +1,14 @@
+package com.blog.demo.config;
+
+import com.blog.demo.api.dto.auth.StringToOauthProviderConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OauthConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToOauthProviderConverter());
+    }
+}
