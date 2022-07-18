8e8da1c8ddc86b6fd890107f8fe592d40c2b558e
lsh23 <shseoul14@gmail.com>
2022-07-18 22:58:40 +0900

    feat: Auth 관련 exception 추가

--- src/main/java/com/blog/demo/exception/ExceptionCodeAndDetails.java
+++ src/main/java/com/blog/demo/exception/ExceptionCodeAndDetails.java
@@ -12,7 +12,13 @@ public enum ExceptionCodeAndDetails {
 
     NOT_FOUND_ERROR_CODE(HttpStatus.INTERNAL_SERVER_ERROR, "발생한 에러의 에러코드를 찾을 수 없습니다.", NotFoundErrorCodeException.class),
 
-    NOT_FOUND_MEMBER("1001","해당하는 id의 post를 찾을 수 없습니다.",NotFoundMemberException .class),
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND,"해당하는 id의 member를 찾을 수 없습니다.",NotFoundMemberException .class),
+    UNSUPPORTED_OAUTH(HttpStatus.BAD_REQUEST,"지원하지 않는 Oauth provider 입니다.",UnsupportedOauthProviderException.class),
+    UNABLE_TO_GET_ACCESS_TOKEN(HttpStatus.INTERNAL_SERVER_ERROR,"소셜 로그인에 실패했습니다.",UnableToGetTokenResponseFromGithubException.class),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED,"로그인이 필요한 서비스입니다.",TokenExpiredException.class),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED,"로그인이 필요한 서비스입니다.",InvalidTokenException.class),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST,"이미 사용 중인 이메일입니다.",DuplicateEmailException.class),
+    AUTHORIZATION_HEADER_INVOLVED(HttpStatus.UNAUTHORIZED, "로그인이 필요한 서비스입니다.", AuthorizationHeaderUninvolvedException.class),
 
     NOT_FOUND_POST(HttpStatus.NOT_FOUND,"해당하는 id의 post를 찾을 수 없습니다.",NotFoundPostException .class),
     NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND,"해당하는 id의 category를 찾을 수 없습니다.",NotFoundCategoryException .class),
